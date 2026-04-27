51aafdde2902b0c3a515402fb19ffb2d0a0ebd7a
Demon6590 <dima.chernytin@gmail.com>
2026-04-27 13:31:14 +0300

    .

--- Store/Model/ProductContext.cs
+++ Store/Model/ProductContext.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace Store.Model;
@@ -15,38 +14,38 @@ public class ProductContext : DbContext
         _connectionString = connectionString;
         Database.EnsureCreated();
     }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(_connectionString);
     }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Product>()
             .Property(p => p.isDelete)
-            .HasDefaultValue(false); 
+            .HasDefaultValue(false);
     }
-    
+
     public void AddProduct(Product p)
     {
         Products.Add(p);
         SaveChanges();
     }
+
     public void SoftDelete(string article)
     {
         var product = Products.Find(article);
         var updated = product with { isDelete = true };
         Entry(product).CurrentValues.SetValues(updated);
         SaveChanges();
-
-        
     }
-    
+
     public void UpdateProduct(Product p)
     {
         var existing = Products.Find(p.Article);
         Entry(existing).CurrentValues.SetValues(p);
         SaveChanges();
-
     }
 
     public IEnumerable<Product> GetAll()
@@ -61,13 +60,13 @@ public class ProductContext : DbContext
             .Where(p => p.Name == name && !p.isDelete);
     }
 
-    
+
     public IEnumerable<Product> GetByPrice(decimal price)
     {
         return Products
             .Where(p => p.Price == price && !p.isDelete);
     }
-    
+
     public IEnumerable<Product> GetByStock(int stock)
     {
         return Products
--- Store/Program.cs
+++ Store/Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Store.Model;
 
-const string ConnectionString = @"Data Source=C:\Users\college\RiderProjects\Store\store.db;";
+const string connectionString = @"Data Source=C:\Users\college\RiderProjects\Store\store.db;";
 
-var db = new ProductContext(ConnectionString);
-db.AddProduct(new Product("r52ff","мандарин","Дерево",134,45,false));
+var db = new ProductContext(connectionString);
+db.AddProduct(new Product("r52ff", "мандарин", "Дерево", 134, 45, false));
 
 var list = db.GetAll().ToList();
 foreach (var product in list)
 {
     Console.WriteLine(product);
 }
+
 Console.WriteLine("---------");
 
-db.UpdateProduct(new Product("r52ff","апельсин","Дерево",120,10,false));
+db.UpdateProduct(new Product("r52ff", "апельсин", "Дерево", 120, 10, false));
 list = db.GetAll().ToList();
 foreach (var product in list)
 {
     Console.WriteLine(product);
 }
+
 Console.WriteLine("---------");
 
 list = db.GetByName("апельсин").ToList();
@@ -28,13 +29,15 @@ foreach (var product in list)
 {
     Console.WriteLine(product);
 }
+
 Console.WriteLine("---------");
 
-list = (List<Product>)db.GetByPrice(120).ToList();
+list = db.GetByPrice(120).ToList();
 foreach (var product in list)
 {
     Console.WriteLine(product);
 }
+
 Console.WriteLine("---------");
 
 list = db.GetByStock(45).ToList();
@@ -42,6 +45,7 @@ foreach (var product in list)
 {
     Console.WriteLine(product);
 }
+
 db.SoftDelete("r52ff");
 Console.WriteLine("---------");
 list = db.GetAll().ToList();
