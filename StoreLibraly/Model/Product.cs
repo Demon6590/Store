59e98b3d6efe657cf711507adbedd9144ce4ee6c
Demon6590 <dima.chernytin@gmail.com>
2026-04-27 13:59:55 +0300

    Добавление проекта библиотеки

---
+++ StoreLibraly/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Model;
+
+[Table("table_products")]
+public record Product(
+    [property: Key]
+    [property: Column("article")] string Article,
+    [property: Column("name")] string Name,
+    [property: Column("manufacturer")] string Manufacturer,
+    [property: Column("price")] decimal Price,
+    [property: Column("stock_quantity")] int StockQuantity,
+    [property: Column("is_delete")] bool isDelete
+);
---
+++ StoreLibraly/Model/ProductContext.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Store.Model;
+
+public class ProductContext : DbContext
+{
+    private readonly string _connectionString;
+    public DbSet<Product> Products { get; set; }
+
+    public ProductContext(string connectionString)
+    {
+        _connectionString = connectionString;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.isDelete)
+            .HasDefaultValue(false);
+    }
+
+    public void AddProduct(Product p)
+    {
+        Products.Add(p);
+        SaveChanges();
+    }
+
+    public void SoftDelete(string article)
+    {
+        var product = Products.Find(article);
+        var updated = product with { isDelete = true };
+        Entry(product).CurrentValues.SetValues(updated);
+        SaveChanges();
+    }
+
+    public void UpdateProduct(Product p)
+    {
+        var existing = Products.Find(p.Article);
+        Entry(existing).CurrentValues.SetValues(p);
+        SaveChanges();
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        return Products
+            .Where(p => !p.isDelete)
+            .ToList();
+    }
+
+    public IEnumerable<Product> GetByName(string name)
+    {
+        return Products
+            .Where(p => p.Name == name && !p.isDelete)
+            .ToList();
+    }
+
+
+    public IEnumerable<Product> GetByPrice(decimal price)
+    {
+        return Products
+            .Where(p => p.Price == price && !p.isDelete)
+            .ToList();
+    }
+
+    public IEnumerable<Product> GetByStock(int stock)
+    {
+        return Products
+            .Where(p => p.StockQuantity == stock && !p.isDelete)
+            .ToList();
+    }
+}
