e4db043b29440a70aac4f79200b4bfc3a9ac7eea
Demon6590 <dima.chernytin@gmail.com>
2026-04-27 10:43:48 +0300

    Добовление дата класа и класс который потключается к бд

---
+++ Store/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Model;
+
+[Table("table_products")]
+public record Product(
+    [property: Key]
+    [property: Column("id")] int Id,
+    [property: Column("name")] string Name,
+    [property: Column("manufacturer")] string Manufacturer,
+    [property: Column("price")] decimal Price,
+    [property: Column("stock_quantity")] int StockQuantity
+);
---
+++ Store/Model/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store.Model;
+
+public class ProductContext : DbContext
+{
+    private readonly string _connectionString;
+    public DbSet<Product> Products { get; set; }
+
+    public ProductContext(string connectionString)
+    {
+        _connectionString = connectionString;
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(_connectionString);
+    }
+}
